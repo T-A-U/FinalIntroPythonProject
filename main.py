da3f8485c6d47af13645b3a2dd09e9070adc1ef5
T-A-U <153134801+T-A-U@users.noreply.github.com>
2025-05-16 23:53:51 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Pokémon Info App
+# Author: Tau Adegun
+# Date: May 2025
+# Description: A Python app with a GUI that shows Pokémon info using a SqLite database and web scraping.
+
+from ui import start_app  # Import the function to launch the GUI
+
+"""
+Run this to start the Pokémon App.
+"""
+
+if __name__ == "__main__":
+    start_app()  # Launch the GUI
+
