0dae7a658220167826ada94bed3d8d3de53fa0c1
T-A-U <153134801+T-A-U@users.noreply.github.com>
2025-05-18 21:32:13 -0700

    Update test_scraper.py

--- test_scraper.py
+++ test_scraper.py
@@ -1,4 +1,5 @@
 #Scraper file was initially returning zeros for HP, attack, and defense so I wrote this to test the output data in the database (via the terminal)
+#Tests if the data outputs expected answer.
 
 from scraper import scrape_pokemon_data
 
